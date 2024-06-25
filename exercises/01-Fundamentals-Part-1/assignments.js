6870485b1130ecc54f74fa7597fc86abbc9c158a
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-06-25 10:37:09 +0200

    Feat: resolve exercise 01

---
+++ exercises/01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,21 @@
+// Values and Variables
+
+const country = "Spain";
+const continent = "Europe";
+let population = 47420000;
+
+console.log(
+  `Country ${country} in continent ${continent} and have ${population} people`
+);
+
+// Data types
+
+const isIsland = false;
+let language;
+
+console.log(
+  `isIsland is a ${typeof isIsland} population is a ${typeof population} country is a ${typeof country} language is a ${typeof language}`
+);
+
+// let, const and Var
+language = "spanish";
---
+++ exercises/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,7 @@
+let js = "amazing";
+console.log(40 + 23 + 8 - 10);
+("Jonas");
+console.log("Jonas");
+let firstName = "Jonas";
+
+// variable name conventions
