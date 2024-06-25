2ed1e633487f1bdc491f80e5d26ddc3485691e8d
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-06-25 19:14:56 +0200

    feat: Resolve exercise 6

--- exercises/06-Modal/starter/script.js
+++ exercises/06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnShowModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
