e87ad71ec2a445539451994e3c28055b3f800332
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-01-11 15:12:06 +0100

    Feat: examples of map method use

--- exercises/11-Arrays-Bankist/starter/lectures.js
+++ exercises/11-Arrays-Bankist/starter/lectures.js
@@ -47,24 +47,40 @@
 // console.log(letters.join(' - '));
 
 // at method
-const arr = [23, 11, 64];
-console.log(arr[0]);
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
 
-console.log(arr.at(-1));
-console.log(arr.at(-2));
+// console.log(arr.at(-1));
+// console.log(arr.at(-2));
 
-// foreach method
+// // foreach method
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// movements.forEach((move, i) =>
+//   move > 0
+//     ? console.log(`Movement ${i + 1}: you deposited ${move} `)
+//     : console.log(`Movement ${i + 1}: you withdrew ${Math.abs(move)} `)
+// );
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+const dollarsMovements = movements.map(mov => mov * eurToUsd);
 
-movements.forEach((move, i) =>
-  move > 0
-    ? console.log(`Movement ${i + 1}: you deposited ${move} `)
-    : console.log(`Movement ${i + 1}: you withdrew ${Math.abs(move)} `)
+console.log(movements);
+console.log(dollarsMovements);
+
+const MovementsDescriptions = movements.map(
+  (move, i) =>
+    `Movement ${i + 1}: you ${move > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      move
+    )}`
 );
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+console.log(MovementsDescriptions);
