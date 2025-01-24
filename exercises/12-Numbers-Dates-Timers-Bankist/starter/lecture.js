f158d34ff4d0967194afe41eedfcf5ca4f3f82e8
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-01-24 22:43:12 +0100

    feat: Add mathematical operations and examples in lecture.js

--- exercises/12-Numbers-Dates-Timers-Bankist/starter/lecture.js
+++ exercises/12-Numbers-Dates-Timers-Bankist/starter/lecture.js
@@ -24,3 +24,19 @@ console.log(randomInt(2, 6));
 console.log(randomInt(2, 6));
 console.log(randomInt(2, 6));
 console.log(randomInt(2, 6));
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+// reminder operator
+console.log(5 % 2);
