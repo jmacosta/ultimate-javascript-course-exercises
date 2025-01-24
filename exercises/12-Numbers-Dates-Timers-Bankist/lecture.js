5ced6d15d60f2c11d9b6a2d691512f3cdfe03bfe
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-01-24 20:45:52 +0100

    feat: Add random number generation functions for dice rolls in lecture.js

---
+++ exercises/12-Numbers-Dates-Timers-Bankist/lecture.js
@@ -0,0 +1,26 @@
+Math.trunc(Math.random() * 6);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+console.log(Math.trunc(Math.random() * 6) + 1);
+console.log(Math.trunc(Math.random() * 6) + 1);
+console.log(Math.trunc(Math.random() * 6) + 1);
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
+console.log(randomInt(2, 6));
