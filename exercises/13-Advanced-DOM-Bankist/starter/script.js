2958c22a56baeb2d0d0b12ff356cc097ce5320a3
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-02-06 20:07:45 +0100

    feat: Implement lazy loading for images using Intersection Observer

--- exercises/13-Advanced-DOM-Bankist/starter/script.js
+++ exercises/13-Advanced-DOM-Bankist/starter/script.js
@@ -152,6 +152,27 @@ allSections.forEach(section => {
   section.classList.add('section--hidden');
 });
 
+// lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
+
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
