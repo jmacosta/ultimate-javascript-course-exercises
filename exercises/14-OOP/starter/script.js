c46ef69bd0c0fbdbac4f418d13cac8c151ef6987
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-02-09 21:28:47 +0100

    feat: Implement Person constructor and prototype method for age calculation

--- exercises/14-OOP/starter/script.js
+++ exercises/14-OOP/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+const Person = function (firstName, birthYear) {
+  //Instances properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  //   this.calcAge = function () {
+  //     console.log(new Date.now() - this.birthYear);
+  //   };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1.- New empty{} is created
+// 2.- function is called, this ={}
+// 3.- {} linked to prototype
+//5.- function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+console.log();
+
+// Prototypes
+
+Person.prototype.calcAge = function () {
+  console.log(2025 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
