d9e8c9bb56fa64894e195cb4af1df54112193904
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-02-09 22:11:17 +0100

    feat: Add unique method to Array prototype and log prototype chain details

--- exercises/14-OOP/starter/script.js
+++ exercises/14-OOP/starter/script.js
@@ -29,3 +29,18 @@ Person.prototype.calcAge = function () {
 
 jonas.calcAge();
 matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 4, 6, 2, 8, 3, 2];
+console.log(arr.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
