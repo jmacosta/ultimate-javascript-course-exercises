892954b70c9c6c7a648b7816750943970dceb66e
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-03-03 22:07:02 +0100

    refactor: Comment out test code and implement wait function for promise handling

--- exercises/16-Asynchronous/starter/script.js
+++ exercises/16-Asynchronous/starter/script.js
@@ -131,11 +131,59 @@ btn.addEventListener('click', function () {
 //     });
 // };
 
-console.log('¡Test start');
-setTimeout(() => console.log('0 sec timer!'), 0);
-Promise.resolve('Resolve promise 1').then(res => console.log(res));
-Promise.resolve('Resolve promise 2').then(res => {
-  for (let i = 0; i < 100000000000; i++) {}
-  console.log(res);
-});
-console.log('Test end');
+// console.log('¡Test start');
+// setTimeout(() => console.log('0 sec timer!'), 0);
+// Promise.resolve('Resolve promise 1').then(res => console.log(res));
+// Promise.resolve('Resolve promise 2').then(res => {
+//   for (let i = 0; i < 1000; i++) {}
+//   console.log(res);
+// });
+// console.log('Test end');
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lotter draw is happening 🔮');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You WIN 🤑💰');
+//     } else {
+//       reject('You lost your money 💩');
+//     }
+//   }, 2000);
+// });
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifing setTimeout
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// wait(2)
+//   .then(() => {
+//     console.log('I waited for 2 seconds');
+//     return wait(1);
+//   })
+//   .then(() => console.log('I waited for 1 second'));
+
+wait(1)
+  .then(() => {
+    console.log('I waited for 1 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 3 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 4 seconds');
+  });
+
+Promise.resolve('ABC').then(x => console.log(x));
+Promise.reject('ABC').then(x => console.error(x));
